4aabc32522e2c9d7510882d0af56b82f094e8023
Vincent Lonij <29819815+vincentropy@users.noreply.github.com>
2017-11-08 19:04:49 +0000

    moved from core to separate repo

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {  Jumbotron } from 'react-bootstrap';
+
+
+const About = () =>(
+  <div className="container">
+
+  <Jumbotron>
+  <h2>Build,  Deploy,  Win.</h2>
+  <p>This is the About Page</p>
+
+  </Jumbotron>
+  </div>
+)
+
+export default About
