27a6eaeba8aaa8e71c33d3ff3037441bfc18cafb
Vincent Lonij <29819815+vincentropy@users.noreply.github.com>
2018-06-10 23:28:28 +0100

    added link to file-upload page.

--- src/components/FileUploadForm.js
+++ src/components/FileUploadForm.js
@@ -3,53 +3,42 @@ import {connect} from "react-redux"
 import {Form, Input} from 'semantic-ui-react'
 import {uploadFormChange, uploadFormSubmit} from '../actions/CodeUpload.js'
 
-// const file_change = (e) => {
-//   var selectedFile = e.files[0]
-//   var reader = new FileReader();
-//   //reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-//   reader.onload = ((data) => {console.log(data.target.result);} )
-//   reader.readAsText(selectedFile);
-//   // console.log(selectedFile.name);
-//   // console.log(selectedFile.file);
-//   // console.log(e)
-//   // console.log(e.type)
-//   // console.log()
-// }
 
 class FileUploadForm extends Component {
   constructor(props) {
     super(props);
     this.fileRef = React.createRef();
   }
-render() {
-  var {formData, didSubmit, onChange, onSubmit} = this.props
-return (
-  <Form onSubmit={() => onSubmit(formData)}>
-    <Form.Input
-      value={formData.gameType}
-      type='text'
-      placeholder='Choose Game Type'
-      name='gameType'
-      label='Game Type'
-      onChange={ (e) => onChange(e.target)} />
 
-    <Form.Input
-      value={formData.agentName}
-      type='text'
-      placeholder='Enter a name for your agent'
-      name='agentName'
-      label='Agent Name'
-      onChange={ (e) => onChange(e.target)} />
+  render() {
+    var {formData, didSubmit, onChange, onSubmit} = this.props
+    return (
+      <Form onSubmit={() => onSubmit(formData)}>
+        <Form.Input
+          value={formData.gameType}
+          type='text'
+          placeholder='Choose Game Type'
+          name='gameType'
+          label='Game Type'
+          onChange={ (e) => onChange(e.target)} />
 
-    <Form.Field>
-      <label>Choose a File</label>
-      <Input type='file' name='file' id='fileInput' ref={this.fileRef} onChange={ (e) => onChange(this.fileRef.current.inputRef)}/>
-    </Form.Field>
-    <Form.Button active={didSubmit}>Submit</Form.Button>
-  </Form>
+        <Form.Input
+          value={formData.agentName}
+          type='text'
+          placeholder='Enter a name for your agent'
+          name='agentName'
+          label='Agent Name'
+          onChange={ (e) => onChange(e.target)} />
 
-)
-}
+        <Form.Field>
+          <label>Choose a File</label>
+          <Input type='file' name='file' id='fileInput' ref={this.fileRef} onChange={ (e) => onChange(this.fileRef.current.inputRef)}/>
+        </Form.Field>
+        <Form.Button active={didSubmit}>Submit</Form.Button>
+      </Form>
+
+    )
+  }
 }
 
 const mapStateToProps = state => {
--- src/screens/CodeUpload.js
+++ src/screens/CodeUpload.js
@@ -7,6 +7,8 @@ const CodeUpload =() => (
   <Segment vertical>
     <Container>
       <Header>Join the fun, upload your player code.</Header>
+      <p>Need some help getting started?
+        Checkout the <a href='https://github.com/arenarium/battleground_agent_template'>agent templates.</a></p>
       <Segment>
         <FileUploadForm/>
         <Divider/>
