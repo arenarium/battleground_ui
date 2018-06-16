b241f15f137d5c7b12017941a1d431d672dcb434
Vincent Lonij <29819815+vincentropy@users.noreply.github.com>
2018-06-16 22:37:28 +0100

    Added link to docs.

--- src/screens/CodeUpload.js
+++ src/screens/CodeUpload.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {Segment, Container, Header, Divider} from 'semantic-ui-react'
+import {Segment, Container, Header, Divider, Message} from 'semantic-ui-react'
 import FileUploadForm from '../components/FileUploadForm'
 import CodePreview from '../components/CodePreview'
 
 const CodeUpload =() => (
   <Segment vertical>
     <Container>
-      <Header>Join the fun, upload your player code.</Header>
-      <p>Need some help getting started?
-        Checkout the <a href='https://github.com/arenarium/battleground_agent_template'>agent templates.</a></p>
+      <Header>
+        Join the fun, upload your player code.
+      </Header>
+      <Message info>
+        <Message.Header>
+          Need some help getting started?
+        </Message.Header>
+        <p>
+          Dowload the&nbsp;
+          <a href='https://github.com/arenarium/battleground_agent_template'>
+            Agent Templates,&nbsp;
+          </a>
+          and check out the&nbsp;
+          <a href='https://arenarium.readthedocs.io/'>
+            Getting Started Guide.
+          </a>
+        </p>
+      </Message>
       <Segment>
         <FileUploadForm/>
         <Divider/>
